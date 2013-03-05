99eb00d21436136ed3f384288c38a12d0324b36e
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2013-03-05 10:03:41 +0100

    Updated ruby script in lib with the correct core executable

--- lib/VU-tree-tagger_kernel.rb
+++ lib/VU-tree-tagger_kernel.rb
@@ -6,7 +6,7 @@ module Opener
 
       		class Configuration
         		CORE_DIR    = File.expand_path("../core", File.dirname(__FILE__))
-        		KERNEL_CORE = CORE_DIR+'/treetagger.py'
+        		KERNEL_CORE = CORE_DIR+'/tt_from_kaf_to_kaf.py'
       		end
     	end
     end
