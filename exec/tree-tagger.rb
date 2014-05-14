6b8f2c227217570644424fa6c45b2f0d22bd3b7a
Yorick Peterse <yorickpeterse@olery.com>
2014-05-14 14:06:22 +0200

    Use require_relative in the exec/ directory.
    This allows you to run the daemon without having to install it as a Gem.

--- exec/tree-tagger.rb
+++ exec/tree-tagger.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
-#
+
 require 'opener/daemons'
-require 'opener/tree_tagger'
+require_relative '../lib/opener/tree_tagger'
 
 options = Opener::Daemons::OptParser.parse!(ARGV)
-daemon = Opener::Daemons::Daemon.new(Opener::TreeTagger, options)
-daemon.start
+daemon  = Opener::Daemons::Daemon.new(Opener::TreeTagger, options)
 
+daemon.start
